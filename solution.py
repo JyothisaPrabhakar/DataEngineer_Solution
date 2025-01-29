25d0ea8ac142b9098160a6356322adb14baaf367
JyothisaPrabhakar <jprabakaar@gmail.com>
2025-01-29 07:28:10 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,3 +3,12 @@ from airflow.operators.python import PythonOperator
 from datetime import datatime, timedelta
 import pandas as pd
 import os
+
+#IO paths
+input_directory = "path/input
+output_file = "path/output/solution.csv"
+
+#transform json data to tabular format
+def transform_data_to_csv():
+  
+  
